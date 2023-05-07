ee8a60fc8c233f1ce0791877e774cab65f75287d
Alejandro <40213661+Olliejandro@users.noreply.github.com>
2023-05-06 20:12:38 -0700

    Fixed minor errors, Where JS code was incomplete.

--- script.js
+++ script.js
@@ -51,24 +51,83 @@ function createList() {
         myul.appendChild(li);
     });
 };
+// Creating Changes for the branch
 
-let countdowns = document.querySelectorAll('.restaurant-countdown');
-      countdowns.forEach(countdown => {
-        let closingTime = new Date();
-        let closingHours = parseInt(countdown.getAttribute('data-time').split(':')[0]);
-        let closingMinutes = parseInt(countdown.getAttribute('data-time').split(':')[1]);
-        let closingSeconds = parseInt(countdown.getAttribute('data-time').split(':')[2]);
-        closingTime.setHours(closingHours, closingMinutes, closingSeconds);
-        
-        let timeRemaining = closingTime - new Date();
-        let hours = Math.floor(timeRemaining / (1000 * 60 * 60));
-        let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-        
-        countdown.innerText = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-        
-        setInterval(() => {
-          timeRemaining = closingTime - new Date();
-          hours = Math.floor(timeRemaining / (1000 * 60 * 60));
-          minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-          seconds = Math.floor((time
+
+// creating a restaurant array
+const restaurants = [
+    { name: "Restaurant 1 test array", closingTime: "20:00:00" },
+    { name: "Restaurant 2 test array", closingTime: "22:00:00" },
+    { name: "Restaurant 3 test array", closingTime: "21:00:00" }
+  ];
+  //Creating a restaurant Class
+  const restaurantContainer = document.querySelector("#new-restaurants");
+  //copying prof format for Creating an element from array 
+  restaurants.forEach((restaurant) => {
+    const restaurantDiv = document.createElement("div");
+    restaurantDiv.classList.add("restaurant-saved");
+  // Googling how to add a saved class 
+    //copying for each item 
+    const restaurantInfoDiv = document.createElement("div");
+    restaurantInfoDiv.classList.add("restaurant-info");
+    restaurantDiv.appendChild(restaurantInfoDiv);
+  
+    const restaurantNameDiv = document.createElement("div");
+    restaurantNameDiv.classList.add("restaurant-name-saved");
+    restaurantNameDiv.innerText = restaurant.name;
+    restaurantInfoDiv.appendChild(restaurantNameDiv);
+  
+    const restaurantHoursDiv = document.createElement("div");
+    restaurantHoursDiv.classList.add("restaurant-hours-saved");
+    restaurantHoursDiv.innerText = `Closing Time: ${restaurant.closingTime}`;
+    restaurantInfoDiv.appendChild(restaurantHoursDiv);
+  
+    const countdownDiv = document.createElement("div");
+    countdownDiv.classList.add("restaurant-countdown");
+    countdownDiv.setAttribute("data-time", restaurant.closingTime);
+    restaurantDiv.appendChild(countdownDiv);
+  //cheating here and appending it... Might not be right but hack and saw 
+    restaurantContainer.appendChild(restaurantDiv);
+  });
+  
+  /// Resturants countdowns:
+  
+  let countdowns = document.querySelectorAll(".restaurant-countdown");
+  countdowns.forEach((countdown) => {
+    var now = new Date();
+    let closingTime = new Date();
+    let closingHours = parseInt(
+      countdown.getAttribute("data-time").split(":")[0]
+    );
+    let closingMinutes = parseInt(
+      countdown.getAttribute("data-time").split(":")[1]
+    );
+    let closingSeconds = parseInt(
+      countdown.getAttribute("data-time").split(":")[2]
+    );
+    closingTime.setHours(closingHours, closingMinutes, closingSeconds);
+  
+    let timeRemaining = closingTime - new Date();
+    let hours = Math.floor(timeRemaining / (1000 * 60 * 60));
+    let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+  
+    countdown.innerText = `${hours
+      .toString()
+      .padStart(2, "0")}:${minutes
+      .toString()
+      .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  
+    setInterval(() => {
+      timeRemaining = closingTime - new Date();
+      hours = Math.floor(timeRemaining / (1000 * 60 * 60));
+      minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+      seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+      countdown.innerText = `${hours
+        .toString()
+        .padStart(2, "0")}:${minutes
+        .toString()
+        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    }, 1000);
+  });
+  
