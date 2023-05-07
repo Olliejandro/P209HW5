7f8c98b6a22b7d86a7204dfef6f3e252e6868bce
Alejandro <40213661+Olliejandro@users.noreply.github.com>
2023-05-07 11:35:44 -0700

    Made changes to minor fixes of adding function

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let NoteObject = function (pData, pType, pPriority) {
     this.type = pType;
     this.priority = pPriority;
 };
-const restaurants = [
+var restaurants = [
   { name: "Restaurant 1 test array", closingTime: "20:00:00" },
   { name: "Restaurant 2 test array", closingTime: "22:00:00" },
   { name: "Restaurant 3 test array", closingTime: "21:00:00" }
@@ -22,12 +22,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
       closingTime: document.getElementById("closingTimeInput").value
     };
     restaurants.push(newRestaurant);
-
+    console.log(restaurants)
     // Clear inputs
     document.getElementById("restaurantName").value = "";
-    document.getElementById("closingTimeInput").value = "";
+    document.getElementById("closingTimeInput").value = ""
+    renderRestaurant(restaurants);
   });
 });
+
 /*
 // runs  when dom is loaded
 document.addEventListener("DOMContentLoaded", function (event) {
@@ -67,39 +69,46 @@ function createList() {
 };
 // Creating Changes for the branch
 
-
+//making change due to VS code dumb editing.
 // creating a restaurant array
 
-  //Creating a restaurant Class
-  const restaurantContainer = document.querySelector("#new-restaurants");
-  //copying prof format for Creating an element from array 
-  restaurants.forEach((restaurant) => {
-    const restaurantDiv = document.createElement("div");
-    restaurantDiv.classList.add("restaurant-saved");
-  // Googling how to add a saved class 
-    //copying for each item 
-    const restaurantInfoDiv = document.createElement("div");
-    restaurantInfoDiv.classList.add("restaurant-info");
-    restaurantDiv.appendChild(restaurantInfoDiv);
-  
-    const restaurantNameDiv = document.createElement("div");
-    restaurantNameDiv.classList.add("restaurant-name-saved");
-    restaurantNameDiv.innerText = restaurant.name;
-    restaurantInfoDiv.appendChild(restaurantNameDiv);
-  
-    const restaurantHoursDiv = document.createElement("div");
-    restaurantHoursDiv.classList.add("restaurant-hours-saved");
-    restaurantHoursDiv.innerText = `Closing Time: ${restaurant.closingTime}`;
-    restaurantInfoDiv.appendChild(restaurantHoursDiv);
-  
-    const countdownDiv = document.createElement("div");
-    countdownDiv.classList.add("restaurant-countdown");
-    countdownDiv.setAttribute("data-time", restaurant.closingTime);
-    restaurantDiv.appendChild(countdownDiv);
-  //cheating here and appending it... Might not be right but hack and saw 
-    restaurantContainer.appendChild(restaurantDiv);
-  });
-  
+
+  function renderRestaurant(restaurants)
+  {
+    //Creating a restaurant Class
+    let restaurantContainer = document.querySelector("#new-restaurants");
+    console.log(restaurants);
+    //copying prof format for Creating an element from array 
+    restaurants.forEach((restaurant) => {
+        let restaurantDiv = document.createElement("div");
+        restaurantDiv.classList.add("restaurant-saved");
+        // Googling how to add a saved class 
+        //copying for each item 
+        let restaurantInfoDiv = document.createElement("div");
+        restaurantInfoDiv.classList.add("restaurant-info");
+        restaurantDiv.appendChild(restaurantInfoDiv);
+      
+        let restaurantNameDiv = document.createElement("div");
+        restaurantNameDiv.classList.add("restaurant-name-saved");
+        restaurantNameDiv.innerText = restaurant.name;
+        restaurantInfoDiv.appendChild(restaurantNameDiv);
+      
+        let restaurantHoursDiv = document.createElement("div");
+        restaurantHoursDiv.classList.add("restaurant-hours-saved");
+        restaurantHoursDiv.innerText = `Closing Time: ${restaurant.closingTime}`;
+        restaurantInfoDiv.appendChild(restaurantHoursDiv);
+      
+        let countdownDiv = document.createElement("div");
+        countdownDiv.classList.add("restaurant-countdown");
+        countdownDiv.setAttribute("data-time", restaurant.closingTime);
+        restaurantDiv.appendChild(countdownDiv);
+      //cheating here and appending it... Might not be right but hack and saw 
+        restaurantContainer.appendChild(restaurantDiv);
+    });
+    console.log(restaurantContainer);
+  }
+
+
   /// Resturants countdowns:
   
   let countdowns = document.querySelectorAll(".restaurant-countdown");
